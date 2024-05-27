bcc5bcfa7e827c59a18342561c061b68170d82f5
SK SAAHIL ALI <130829366+SKSALI1@users.noreply.github.com>
2024-05-27 19:49:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const CsvModel = require('./model/csvModel')
 const mongoose = require('mongoose');
 const { type } = require('os');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const upload = multer({ dest: 'uploads/' });
 
 
